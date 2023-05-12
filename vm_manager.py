a47c3ccb892b9e57b9d8ab579e01018ebe7a7944
ksylik <ksylik@gmail.com>
2023-05-12 08:51:13 +0300

    added logs

--- vm_manager.py
+++ vm_manager.py
@@ -6,6 +6,13 @@ import yaml  # PyYaml 5.4.1
 import os
 import time
 import warnings
+import logging
+import tempfile
+
+# log file in TEMP folder
+log_path = tempfile.gettempdir()
+log = "vm_manager.log"
+full_log_path = os.path.join(log_path, log)
 
 current_dir = os.path.dirname(os.path.abspath(__file__)) + "\\"
 
@@ -31,9 +38,10 @@ def get_json_vcenter_data(json_file_path):
     return vcenter_host, vcenter_user, vcenter_password
 
 
-def get_input_vms(data_json):
+def get_input_vms(data_json, log_file):
     """
     Get all required data about VMs (including excluded VMs)
+    :param log_file: str: full path to log (TEMP)
     :param data_json: str: full path to json
     :return: dict: VM's names as keys, VM's power statuses as values
     """
@@ -47,13 +55,34 @@ def get_input_vms(data_json):
         if vm_name not in data["excludes"]:
             vm_dict[vm_name] = vm_status
         else:
-            print("Machine " + vm_name + " is excluded.")
+            make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " is excluded.")
+            # print("Machine " + vm_name + " is excluded.")
     return vm_dict
 
 
-def main(json_file):
+def make_log(log_file, lev=None, error="", info="", warning=""):
+    logger = logging.getLogger("vm_manager")
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        file_handler = logging.FileHandler(log_file, mode="a")
+        file_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    if lev == logging.INFO:
+        logger.info(info)
+    elif lev == logging.ERROR:
+        logger.error(error)
+    elif lev == logging.WARNING:
+        logger.warning(warning)
+
+
+def main(json_file, log_file):
     """
     Manage VM's by names from json file to turn them on/off
+    :param log_file: str: full path to log (TEMP)
     :param json_file: str: full path to json
     """
 
@@ -67,7 +96,7 @@ def main(json_file):
     content = si.RetrieveContent()
 
     # get VM's data
-    vm_dict = get_input_vms(json_file)
+    vm_dict = get_input_vms(json_file, log_file)
 
     # get all current available VM's from vCenter
     vmview = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
@@ -79,19 +108,48 @@ def main(json_file):
         if res:
             vm = res[0]
             if vm.runtime.powerState == vim.VirtualMachinePowerState.poweredOff and vm_status == "on":
-                print("Machine " + vm_name + " gets power on signal.")
+                make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " gets power on signal.")
+
                 task = vm.PowerOn()
-                while task.info.state not in [vim.TaskInfo.State.success, vim.TaskInfo.State.error]:
-                    time.sleep(1)
-                print("Machine " + vm_name + " turned on.")
+
+                if task:
+                    while task.info.state not in [vim.TaskInfo.State.success, vim.TaskInfo.State.error]:
+                        time.sleep(1)
+                    if task.info.state == vim.TaskInfo.State.success:
+                        make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " powered on.")
+                    elif vim.TaskInfo.State.error:
+                        make_log(log_file, lev=logging.ERROR, error=str(task.info.error) + " for " + vm_name + " machine.")
+                    else:
+                        make_log(log_file, lev=logging.ERROR, error="Unexpected behavior for " + vm_name + " machine while powering on.")
+                else:
+                    make_log(log_file, lev=logging.ERROR, error="Cannot create task for " + vm_name + " machine.")
+
             elif vm.runtime.powerState == vim.VirtualMachinePowerState.poweredOn and vm_status == "on":
-                print("Machine " + vm_name + " is already turned on.")
+                make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " is already powered on.")
+
             elif vm.runtime.powerState == vim.VirtualMachinePowerState.poweredOn and vm_status == "off":
-                print("Machine " + vm_name + " gets shutdown signal.")
+                make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " gets shutdown signal.")
+
+                # nice mode
                 task = vm.ShutdownGuest()
-                print("Machine " + vm_name + " should be turned off.")
+                # hard mode
+                # task = vm.PowerOff()
+
+                if task:
+                    while task.info.state not in [vim.TaskInfo.State.success, vim.TaskInfo.State.error]:
+                        time.sleep(1)
+                    if task.info.state == vim.TaskInfo.State.success:
+                        make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " powered off.")
+                    elif vim.TaskInfo.State.error:
+                        make_log(log_file, lev=logging.ERROR, error=str(task.info.error) + " for " + vm_name + " machine.")
+                    else:
+                        make_log(log_file, lev=logging.ERROR, error="Unexpected behavior for " + vm_name + " machine while powering off.")
+                else:
+                    make_log(log_file, lev=logging.ERROR, error="Cannot create task for " + vm_name + " machine.")
+
             elif vm.runtime.powerState == vim.VirtualMachinePowerState.poweredOff and vm_status == "off":
-                print("Machine " + vm_name + " is already turned off.")
+                make_log(log_file, lev=logging.INFO, info="Machine " + vm_name + " is already powered off.")
+
         else:
             print("Virtual machine " + vm_name + " wasn't found.")
     # release resources
@@ -103,6 +161,6 @@ def main(json_file):
 
 if __name__ == '__main__':
     try:
-        main(json_config_path)
+        main(json_config_path, full_log_path)
     except Exception as e:
-        print(e.args)
+        make_log(log_file, lev=logging.ERROR, error=str(e.args))
